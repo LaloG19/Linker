4ada8cd81708923c04b8df2b0e00b2f8f6ee77f6
LaloG19 <21362134@utcancun.edu.mx>
2024-10-25 02:43:02 -0500

    feat: Register validation

--- src/modules/Register/components/RegisterForm.vue
+++ src/modules/Register/components/RegisterForm.vue
@@ -2,6 +2,7 @@
   <div class="flex w-full justify-center items-center flex-col gap-4">
     <input v-model="email" placeholder="Email" class="border p-2" />
     <input v-model="password" type="password" placeholder="Password" class="border p-2" />
+    <p v-if="message" class="text-red-500">{{ message }}</p>
     <button @click="handleRegister" class="bg-green-500 text-white px-4 py-2">Register</button>
   </div>
 </template>
@@ -13,8 +14,26 @@ import { useAuth } from '@/composables/useAuth';
 const email = ref('');
 const password = ref('');
 const { register } = useAuth();
+const respuesta = ref('');
+const message = ref('');
 
 const handleRegister = async () => {
-  await register(email.value, password.value);
+  respuesta.value = await register(email.value, password.value);
+  
+  if(respuesta.value.user?.email)
+  {
+    alert('Usuario registrado');
+  }else if(respuesta.value.code == 'auth/email-already-in-use'){
+    message.value = 'El correo ya existe';
+  }else if (respuesta.value.code == 'auth/weak-password'){
+    message.value = 'La contraseña debe tener al menos 6 caracteres';
+  }
+
+  clearForm();
+};
+
+const clearForm = () => {
+  email.value = '';
+  password.value = '';
 };
 </script>
